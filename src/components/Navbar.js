a79ba63f67a9f11a91deb9b0d7156944f39a9486
Jucimeire Silva <jucimeiresilva@hotmail.com>
2022-04-30 11:33:43 -0300

    teste

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,13 @@ const NavBar = () => {
   return (
     <nav className="navbar">
         <div className="nav-links d-flex justify-content-between align-items-center">
-          <NavLink to="/home" className="link home">
+          <NavLink to="/" className="link home">
             <div className="nav-home subtitle">Home</div>
           </NavLink>
           <NavLink to="/cadastro" className="link cadastro">
             <div className="nav-cadastro subtitle">Cadastro</div>
           </NavLink>
-          <p> | </p>
-          <NavLink to="/" className="link menu">
+          <NavLink to="/menu" className="link menu">
             <div className="nav-menu">Menu</div>
           </NavLink>
         </div>
