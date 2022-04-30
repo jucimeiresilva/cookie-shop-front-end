e732ba1f0bf0970ac569c893e20513da170532ce
Jucimeire Silva <jucimeiresilva@hotmail.com>
2022-04-29 21:13:30 -0300

    navbar em andamento

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-
 const NavBar = () => {
-    return (
-        <nav className="navbar">
-            <div className="container d-flex align-items-center">
-                <NavLink to="/" className="link">
-                    <img className="logo" src={logo} alt="site logo" />
-                </NavLink>
-            </div>
-            <div className="nav-links d-flex justify-content-between align-items-center">
-                <NavLink to="/home" className="link home">
-                    <div className="nav-home subtitle">Home</div>
-                </NavLink>
-                <NavLink to="/login" className="link login">
-                    <div className="nav-login" subtitle>Login</div>
-                </NavLink>
-                <p> | </p>
-                <NavLink to="/menu" className="link subtitle">
-                    <div className="nav-menu">Menu</div>
-                </NavLink>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+        <div className="nav-links d-flex justify-content-between align-items-center">
+          <NavLink to="/home" className="link home">
+            <div className="nav-home subtitle">Home</div>
+          </NavLink>
+          <NavLink to="/cadastro" className="link cadastro">
+            <div className="nav-cadastro subtitle">Cadastro</div>
+          </NavLink>
+          <p> | </p>
+          <NavLink to="/" className="link menu">
+            <div className="nav-menu">Menu</div>
+          </NavLink>
+        </div>
+    </nav>
+  );
 };
 
 export default NavBar;
